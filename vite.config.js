3192331162ffcea80783c7d145bb3dcb7a00cfcd
basii199 <ubokabasi135@gmail.com>
2025-04-30 10:46:08 +0100

    fix deployment paths

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  base: "/HNG-12-FE-AI-TRANSLATOR",
+  base: "/ai-translator",
 })
